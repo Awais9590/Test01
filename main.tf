61d6a270438b2e1083f35fc9cbf25569fdd62c87
Awais9590 <139554438+Awais9590@users.noreply.github.com>
2023-07-18 19:51:05 +0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -11,205 +11,188 @@ terraform {
 provider "azurerm" {
   features {}
 }
+resource "azurerm_resource_group" "sqlvm" {
+  name     = "sqlvm"
+  location = "West Europe"
+}
 
-resource "azurerm_resource_group" "mtc-rg" {
-  name     = var.resource_group_name
-  location = "East US"
+resource "azurerm_virtual_network" "vnetsql" {
+  name                = "vnetforsql"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.sqlvm.location
+  resource_group_name = azurerm_resource_group.sqlvm.name
+}
+
+resource "azurerm_subnet" "subnetsql" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.sqlvm.name
+  virtual_network_name = azurerm_virtual_network.vnetsql.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_public_ip" "ip1" {
+  name                = "PublicIP1"
+  location            = azurerm_resource_group.sqlvm.location
+  resource_group_name = azurerm_resource_group.sqlvm.name
+  allocation_method   = "Dynamic"
+
   tags = {
-    enviroment = "dev"
+    environment = "sqlvm"
   }
 }
 
-resource "azurerm_virtual_network" "mtc-vn" {
-  name                = var.test_virtual_network
-  resource_group_name = azurerm_resource_group.mtc-rg.name
-  location            = azurerm_resource_group.mtc-rg.location
-  address_space       = ["10.123.0.0/16"]
-  tags = {
-    enviroment = "dev"
+resource "azurerm_network_security_group" "sqlnsg" {
+  name                = "NSG-SQL"
+  location            = azurerm_resource_group.sqlvm.location
+  resource_group_name = azurerm_resource_group.sqlvm.name
+
+  security_rule {
+    name                       = "TCPIN"
+    priority                   = "100"
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
-resource "azurerm_subnet" "test" {
-  count = length(var.subnet_names)
-  name  = var.user_names[count.index]
+resource "azurerm_subnet_network_security_group_association" "nsgnic" {
+  subnet_id                 = azurerm_subnet.subnetsql.id
+  network_security_group_id = azurerm_network_security_group.sqlnsg.id
 }
 
-# resource "azurerm_subnet" "mtc-subnetLinux" {
-#   name                 = var.subnet_name
-#   resource_group_name  = azurerm_resource_group.mtc-rg.name
-#   virtual_network_name = azurerm_virtual_network.mtc-vn.name
-#   address_prefixes     = ["10.123.1.0/24"]
-# }
+//-------------------------------------------------------------------------------
 
-# resource "azurerm_subnet" "mtc-subnetWin" {
-#   name                 = "mtc-subnetWin"
-#   resource_group_name  = azurerm_resource_group.mtc-rg.name
-#   virtual_network_name = azurerm_virtual_network.mtc-vn.name
-#   address_prefixes     = ["10.123.2.0/24"]
-# }
-
-# resource "azurerm_subnet" "mtc-subnetBastion" {
-#   name                 = "AzureBastionSubnet"
-#   resource_group_name  = azurerm_resource_group.mtc-rg.name
-#   virtual_network_name = azurerm_virtual_network.mtc-vn.name
-#   address_prefixes     = ["10.123.3.0/24"]
-# }
-
-resource "azurerm_network_security_group" "mtc-sg" {
-  name                = "mtc-sg"
-  location            = azurerm_resource_group.mtc-rg.location
-  resource_group_name = azurerm_resource_group.mtc-rg.name
-  tags = {
-    enviroment = "dev"
-  }
-}
-
-resource "azurerm_network_security_rule" "mtc-dev-rule" {
-  name                        = "test123"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.mtc-rg.name
-  network_security_group_name = azurerm_network_security_group.mtc-sg.name
-}
-
-resource "azurerm_subnet_network_security_group_association" "mtc-sga" {
-  subnet_id                 = azurerm_subnet.mtc-subnetLinux.id
-  network_security_group_id = azurerm_network_security_group.mtc-sg.id
-}
-
-resource "azurerm_subnet_network_security_group_association" "mtc-sgb" {
-  subnet_id                 = azurerm_subnet.mtc-subnetWin.id
-  network_security_group_id = azurerm_network_security_group.mtc-sg.id
-}
-
-resource "azurerm_public_ip" "mtc-ip" {
-  name                = "mtc-ip"
-  resource_group_name = azurerm_resource_group.mtc-rg.name
-  location            = azurerm_resource_group.mtc-rg.location
-  allocation_method   = "Static"
-  sku                 = "Standard"
-
-
-  tags = {
-    environment = "dev"
-  }
-}
-
-resource "azurerm_network_interface" "mtc-nic" {
-  name                = "mtc-nic"
-  location            = azurerm_resource_group.mtc-rg.location
-  resource_group_name = azurerm_resource_group.mtc-rg.name
+resource "azurerm_network_interface" "sqlnic" {
+  name                = "SQL-NIC"
+  location            = azurerm_resource_group.sqlvm.location
+  resource_group_name = azurerm_resource_group.sqlvm.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.mtc-subnetLinux.id
+    name                          = "config1"
+    subnet_id                     = azurerm_subnet.subnetsql.id
     private_ip_address_allocation = "Dynamic"
-  }
-  tags = {
-    environment = "dev"
+    public_ip_address_id          = azurerm_public_ip.ip1.id
   }
 }
 
-resource "azurerm_network_interface" "mtc-nic2" {
-  name                = "mtc-nic2"
-  location            = azurerm_resource_group.mtc-rg.location
-  resource_group_name = azurerm_resource_group.mtc-rg.name
+resource "azurerm_virtual_machine" "vm1" {
+  name                  = "VM-SQL"
+  location              = azurerm_resource_group.sqlvm.location
+  resource_group_name   = azurerm_resource_group.sqlvm.name
+  network_interface_ids = ["${azurerm_network_interface.sqlnic.id}"]
+  vm_size               = "Standard_D4s_v3"
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.mtc-subnetWin.id
-    private_ip_address_allocation = "Dynamic"
-  }
-  tags = {
-    environment = "dev"
-  }
-}
-
-resource "azurerm_network_interface" "mtc-nic3" {
-  name                = "mtc-nic3"
-  location            = azurerm_resource_group.mtc-rg.location
-  resource_group_name = azurerm_resource_group.mtc-rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.mtc-subnetWin.id
-    private_ip_address_allocation = "Dynamic"
-    # public_ip_address_id          = azurerm_public_ip.mtc-ip.id
-  }
-  tags = {
-    environment = "dev"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "mtc-vm" {
-  name                = "mtc-vm"
-  resource_group_name = azurerm_resource_group.mtc-rg.name
-  location            = azurerm_resource_group.mtc-rg.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.mtc-nic.id,
-  ]
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/mtcazurekey.pub")
-  }
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+  storage_image_reference {
+    offer     = "SQL2016SP1-WS2016"
+    publisher = "MicrosoftSQLServer"
+    sku       = "SQLDEV"
     version   = "latest"
   }
+
+  #   boot_diagnostics {
+  #     enabled     = true
+  #     storage_uri = "https://www.google.com"
+  #   }
+
+  storage_os_disk {
+    name              = "sqlnewOsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  #   storage_data_disk {
+  #     name              = "sqlvm_disk_1"
+  #     managed_disk_type = "Standard_LRS"
+  #     create_option     = "Empty"
+  #     lun               = 0
+  #     disk_size_gb      = "128"
+  #   }
+
+  os_profile {
+    computer_name  = "sqlxvm"
+    admin_username = "adminuser"
+    admin_password = "Pass1234@"
+  }
+
+  os_profile_windows_config {
+    provision_vm_agent = true
+  }
+}
+
+resource "azurerm_managed_disk" "sqlvmdisk1" {
+  name                 = "sqlvm-disk1"
+  location             = azurerm_resource_group.sqlvm.location
+  resource_group_name  = azurerm_resource_group.sqlvm.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 128
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "diskattach" {
+  managed_disk_id    = azurerm_managed_disk.sqlvmdisk1.id
+  virtual_machine_id = azurerm_virtual_machine.vm1.id
+  lun                = 1
+  caching            = "ReadWrite"
+}
+
+resource "azurerm_managed_disk" "sqlvmdisk2" {
+  name                 = "sqlvm-disk2"
+  location             = azurerm_resource_group.sqlvm.location
+  resource_group_name  = azurerm_resource_group.sqlvm.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 128
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "diskattach2" {
+  managed_disk_id    = azurerm_managed_disk.sqlvmdisk2.id
+  virtual_machine_id = azurerm_virtual_machine.vm1.id
+  lun                = 0
+  caching            = "ReadWrite"
+}
+
+//// sql machine configuration below 
+
+resource "azurerm_mssql_virtual_machine" "sqlextension" {
+  virtual_machine_id               = azurerm_virtual_machine.vm1.id
+  sql_license_type                 = "PAYG"
+  r_services_enabled               = true
+  sql_connectivity_port            = 1433
+  sql_connectivity_type            = "PRIVATE"
+  sql_connectivity_update_password = "Password1234!"
+  sql_connectivity_update_username = "sqllogin"
+
+  auto_patching {
+    day_of_week                            = "Sunday"
+    maintenance_window_duration_in_minutes = 60
+    maintenance_window_starting_hour       = 2
+  }
+
+  storage_configuration {
+    disk_type             = "NEW"
+    storage_workload_type = "GENERAL"
+
+    data_settings {
+      default_file_path = "F:\\data"
+      luns              = [0]
+    }
+
+    log_settings {
+      default_file_path = "G:\\log"
+      luns              = [1]
+    }
+
+    temp_db_settings {
+      default_file_path = "G:\\temp"
+      luns              = [1]
+    }
+  }
 }
 
 
 
-resource "azurerm_windows_virtual_machine" "mtc-vm2" {
-  name                = "mtc-vm2"
-  resource_group_name = azurerm_resource_group.mtc-rg.name
-  location            = azurerm_resource_group.mtc-rg.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser2"
-  admin_password      = "P@$$w0rd1234!"
-  network_interface_ids = [
-    azurerm_network_interface.mtc-nic2.id,
-  ]
-
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    offer     = "sql2019-ws2019"
-    publisher = "MicrosoftSQLServer"
-    sku       = "standard"
-    version   = "15.0.230110"
-  }
-}
-
-resource "azurerm_bastion_host" "mtc-bastion" {
-  name                = "mtc-bastion"
-  location            = azurerm_resource_group.mtc-rg.location
-  resource_group_name = azurerm_resource_group.mtc-rg.name
-
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.mtc-subnetBastion.id
-    public_ip_address_id = azurerm_public_ip.mtc-ip.id
-  }
-}
