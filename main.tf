740b6e52f7861f0d37b8c39139827c75aba00f89
Awais9590 <139554438+Awais9590@users.noreply.github.com>
2023-07-20 15:21:12 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,11 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {
+
      subscription_id = "e2281e72-9c41-4ddd-9d57-fd5adc915686"
      client_id       = "f548d2b4-7d58-47a5-91b2-97c0d3d1db15"
      client_secret   = ""
      tenant_id       = "f548d2b4-7d58-47a5-91b2-97c0d3d1db15"
-  }
 }
 resource "azurerm_resource_group" "sqlvm" {
   name     = "sqlvm"
